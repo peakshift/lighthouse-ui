f6785ff3bf4e76d95308044727db351e12f99aa5
Johns Beharry <johns@peakshift.com>
2020-01-24 00:54:54 +0100

    chore(beacons): add example table view of beacons list

--- src/components/ListBeacons.tsx
+++ src/components/ListBeacons.tsx
@@ -5,7 +5,8 @@ import {
 import AppActions from "../actions/AppActions";
 import AppStore from "../stores/AppStore";
 
-import List from "../components/List";
+import List from "./List";
+import Table from "./Table";
 
 export default class ListBeacons extends React.Component<IAppProps, IAppState> {
 
@@ -45,6 +46,40 @@ export default class ListBeacons extends React.Component<IAppProps, IAppState> {
 	}
 
 	public render() {
+
+        let list = [
+            {
+                name: "Mr Poopy Butthole",
+                peers: "?",
+                validators: "?",
+                syncState: "?",
+                head: "?",
+                finalizedSlot: "?",
+                diskUsage: "?",
+                version: "?",
+            },
+            {
+                name: "shasper-is-back",
+                peers: "?",
+                validators: "?",
+                syncState: "?",
+                head: "?",
+                finalizedSlot: "?",
+                diskUsage: "?",
+                version: "?",
+            },
+            {
+                name: "?",
+                peers: "?",
+                validators: "?",
+                syncState: "?",
+                head: "?",
+                finalizedSlot: "?",
+                diskUsage: "?",
+                version: "?",
+            },
+        ];
+
 		return (
 			<div>
 				<section>
@@ -76,13 +111,316 @@ export default class ListBeacons extends React.Component<IAppProps, IAppState> {
 							<p>123</p>
 						</article>
 					</div>
-					<h4>Total Connected Peers: 540</h4>
-					<p>Connected Valicators: 16,530</p>
-					<button className="button-actions">
-						<Link to="/connect-validator">
-							Connect Validator
-						</Link>
-					</button>
+                    <h6>Filters</h6>
+                    <ul>
+                        <li><a href="">Tags</a></li>
+                    </ul>
+                    <table className="large-staker">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Peers</th>
+                                <th>Validators</th>
+                                <th>Sync State</th>
+                                <th>Head Slot</th>
+                                <th>Finalized slot</th>
+                                <th>Disk Usage</th>
+                                <th>Version</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><Link to="/beacons/view/mr-poopy-butthole">Mr Poopy Butthole</Link></td>
+                                <td>64</td>
+                                <td>256</td>
+                                <td>SYNC_STATE</td>
+                                <td>90,122</td>
+                                <td>90,123</td>
+                                <td>70 gb</td>
+                                <td>Lighthouse/v0.1.0-unstable/x86_64-macos</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>unknown</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>SYNC_STATE</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                                <td>0</td>
+                            </tr>
+                        </tbody>
+                    </table>
 				</section>
 			</div>
 		);
